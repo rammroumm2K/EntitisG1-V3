50b9a34a0f494db0a1e8a52371a079c993c9389b
rammroumm2K <ramybrz00@gmail.com>
2024-09-26 13:02:13 +0200

    rivedere progetto MK

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,22 +10,35 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(SectionRepository $section): Response
     {
         return $this->render(
             'main/index.html.twig', [
                 'title' => 'Homepage',
                 'homepage_text'=> "Nous somme le ".date('d/m/Y \à H:i'
                 ),
+                'section'=> $section->findAll()
             ]
         );
     }
     #[Route('/about', name: 'about_me')]
-    public function aboutMe(): Response
+    public function aboutMe(SectionRepository $section): Response
     {
         return $this->render('main/about.html.twig', [
             'title' => 'About me',
             'homepage_text'=> "Et je parle encore de moi !",
         ]);
     }
+
+// creation de lurl pour le detail d'une section
+/*#[Route('/section/{id}', name: 'section', requirements:  ['id'=> '>d+] defaults: 
+    ['id'=> 1])]
+        public function section(SectionRepository $section, int $id): Response
+    {
+        
+            
+    }
+
 }
+
+*/
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,7 +12,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, SectionRepository $section): Response
     {
 
         // si on est déjà connecté
@@ -29,6 +30,8 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
+            'section'=> $section->findAll()
+            
         ]);
     }
 
